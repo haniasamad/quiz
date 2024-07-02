b2092a81e1f8e65f2778e7dcabcf3d63c9ce340b
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-07-02 20:56:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var score =0;
+var question1 =+ prompt("What is your Country")
+var answer1 = "Pakistan"
+
+if (question1 === answer1){
+score ++
+}
+var question2 = prompt("What is 2 + 3")
+var answer2 = "5"
+
+if (question2 === answer2){
+score ++
+}
+var question3 = prompt("What is 1 + 1")
+var answer3 = "2"
+
+if (question3 === answer3){
+score ++
+}
+var question4 = prompt("What is 4 + 5")
+var answer4 = "9"
+
+if (question4 === answer4){
+score ++
+}
+else{ 
+alert("Loser👎👎")
+}
+alert("your score is" + score)
